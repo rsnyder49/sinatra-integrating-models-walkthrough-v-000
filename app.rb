abdc66c5ef9ffd6015a65db5f0562ecbda1cac75
Robert Snyder <r.snyder49@gmail.com>
2018-11-06 23:16:30 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require_relative 'config/environment'
 require_relative 'models/text_analyzer.rb'
+require_relative 'models/text_analyzer.rb'
 
 class App < Sinatra::Base
   get '/' do
@@ -8,6 +9,8 @@ class App < Sinatra::Base
 
   post '/' do
     text_from_user = params[:user_text]
+    
+    @analyzed_text = TextAnalyzer.new(text_from_user)
 
     erb :results
   end
